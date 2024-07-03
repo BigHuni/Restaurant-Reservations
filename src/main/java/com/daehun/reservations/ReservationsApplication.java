10a252e797a58d040dc87fd70f07e7eddff4d61a
BigHuni <hdh8659@gmail.com>
2024-07-04 04:29:50 +0900

    init

---
+++ src/main/java/com/daehun/reservations/ReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.daehun.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsApplication.class, args);
+	}
+
+}
